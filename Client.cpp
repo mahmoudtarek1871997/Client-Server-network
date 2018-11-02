78a21c076621ff3e5e3a530809da85f99d0c002f
yassmin96 <eng-yassmin.barakat1419@alexu.edu.eg>
2018-11-02 14:38:42 +0200

    clientcheckGet

--- Client.cpp
+++ Client.cpp
@@ -6,39 +6,48 @@
 Client::Client(){}
 bool Client::conToserver(string hostName, int port) {
     soc_desc = socket(AF_INET , SOCK_STREAM , 0);
-     if(soc_desc > 0){
-         server.sin_family = AF_INET;
-         server.sin_port = htons(port);
-         server.sin_addr = getHostIP(hostName);
-         int con = connect(soc_desc, (struct sockaddr *) &server, sizeof(server));
-         return con > 0;
-     }
-     return false;
+    cout << soc_desc <<"client";
+    if(soc_desc > 0){
+        server.sin_family = AF_INET;
+        server.sin_port = htons(port);
+        server.sin_addr = getHostIP(hostName);
+        int con = connect(soc_desc, (struct sockaddr *) &server, sizeof(server));
+        return con > 0;
+    }
+    return false;
 }
 void Client::handleRequest(string method, string fileName) {
-   if(method == "GET"){
-       if (sendHeader("GET "+ fileName + " HTTP/1.1")){
-           recieveData(1500, fileName); //////////////////////////////change
-       }
+    if(method == "GET"){
+        if (sendHeader("GET " + fileName + " HTTP/1.1\r\n\r\n")){
+            cout << recieveData(1500, fileName); //////////////////////////////change
+        }
 
-   }else{
-       if (sendHeader("POST "+ fileName + " HTTP/1.1")){
-           if(recieveData(1500, fileName) == "OK"){ //////////////////////////////change
-               sendFile(fileName);
-           }
-       }
-   }
+    }else{
+        if (sendHeader("POST "+ fileName + " HTTP/1.1")){
+            if(recieveData(1500, fileName) == "OK"){ //////////////////////////////change
+                sendFile(fileName);
+            }
+        }
+    }
 }
 bool Client::sendHeader(string data) {
-    return send(soc_desc , data.c_str() , strlen(data.c_str()) , 0) > 0;
+    int size =  send(soc_desc , data.c_str() , strlen(data.c_str()) , 0);
+    if(size > 0){
+        cout<<"sendH size " << size <<endl;
+        return true;
+    }
+    return false;
+
 }
 string Client::recieveData(int size, string fileName) {   /////////////change can't read the hole file
+
     FILE *fp = fopen(fileName.c_str(),"w");
     char buffer[size];
     string data = "";
     ssize_t recvSize;
     do{
         recvSize = recv(soc_desc , buffer , sizeof(buffer) , 0);
+        cout<<"recvSize " << recvSize << endl;
         data += buffer;
         fwrite(buffer , sizeof(char), sizeof(buffer), fp);
         fflush(fp);
@@ -52,7 +61,7 @@ struct in_addr Client::getHostIP(string hostName) {
 
     if((host = gethostbyname(hostName.c_str())) != NULL){
         in_list = (struct in_addr **)host->h_addr_list;
-        cout<<inet_ntoa(**in_list) <<"\n";
+       // cout<<inet_ntoa(**in_list) <<"\n";
         return **in_list;
     }
 
@@ -68,6 +77,7 @@ void Client::sendFile(string fileName) {
     while (SizeCheck < FileSize) {
         buff = (char *) malloc(1500);
         int Read = fread(buff, 1500, sizeof(char), fp);
+        cout <<"SendF size  " << Read << endl;
         send(soc_desc, buff, Read, 0);
         SizeCheck += Read;
         free(buff);
